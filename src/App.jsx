fa8b95a864d28c5a6207db5f368932265f2af78b
heisraymond <imraymond07@gmail.com>
2026-01-28 10:02:56 +0300

    Splited components into specific files by their feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,19 @@
-import { useState, Fragment } from 'react';
+import { Fragment } from 'react';
 
-import { CORE_CONCEPTS } from './data';
-import { EXAMPLES } from './data.js';
-import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import Examples from './components/Examples.jsx';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+
+
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState();
-
-    function handleSelect(selectedButton) {
-        setSelectedTopic(selectedButton)
-        //console.log(selectedTopic);
-    }
 
     return (
         <Fragment>
             <Header />
             <main>
-                <section id='core-concepts'>
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        {CORE_CONCEPTS.map ((conceptItem) => (
-                            <CoreConcept key={conceptItem.title} {...conceptItem} />
-                        ))}
-                    </ul>
-                </section>
-
-                <section id='examples'>
-                    <h2>Examples</h2>
-                    <menu>
-                        <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-                        <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                        <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-                        <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-                    </menu>
-                    {!selectedTopic ? <p> Please select a button </p> : <div id="tab-content">
-                        <h3> {EXAMPLES[selectedTopic].title} </h3>
-                        <p> {EXAMPLES[selectedTopic].description} </p>
-                        <pre>
-                            <code> {EXAMPLES[selectedTopic].code} </code>
-                        </pre>
-                    </div>}
-
-                </section>
+                <CoreConcepts />
+                <Examples />
             </main>
         </Fragment>
     );
