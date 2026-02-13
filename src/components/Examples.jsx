cad44deaa3e683d26ce65dca072114f9485e79e1
heisraymond <imraymond07@gmail.com>
2026-02-13 18:46:52 +0300

    🔧 UPDATE: Added advanceD react concepts

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import { EXAMPLES } from "../data.js";
+import Section from "./Section.jsx";
 import TabButton from "./TabButton.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -12,22 +14,26 @@ export default function Examples() {
     }
 
     return (
-        <section id='examples'>
-            <h2>Examples</h2>
-            <menu>
+        <Section title='Examples' id='examples'>
+            <Tabs 
+            buttonsContainer='menu'
+            buttons={
+                <>
                 <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
                 <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
                 <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
                 <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-            </menu>
-            {!selectedTopic ? <p> Please select a button </p> : <div id="tab-content">
-                <h3> {EXAMPLES[selectedTopic].title} </h3>
-                <p> {EXAMPLES[selectedTopic].description} </p>
-                <pre>
-                    <code> {EXAMPLES[selectedTopic].code} </code>
-                </pre>
-            </div>}
+                </>
+            }>
+                {!selectedTopic ? <p> Please select a button </p> : <div id="tab-content">
+                    <h3> {EXAMPLES[selectedTopic].title} </h3>
+                    <p> {EXAMPLES[selectedTopic].description} </p>
+                    <pre>
+                        <code> {EXAMPLES[selectedTopic].code} </code>
+                    </pre>
+                </div>}
+            </Tabs>
 
-        </section>
+        </Section>
     )
 }
